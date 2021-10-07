be7a2c554ee37c919e0b3321a3140e1026270152
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-10-08 01:03:16 +0530

    create basic server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+// Middlewares
+// Functions that execute during the lifecycle of a request to the Express server. And we can check the user authentication here.
+// app.use('/users', () => {
+//     console.log("Hello this is users end point!")
+// })
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello guys! how are you?')
+})
+
+app.get('/users', (req, res) => {
+    res.send('This is users page!')
+})
+
+// Start listening to the server'
+app.listen(3300)
