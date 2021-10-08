e737fae24db4299a1c369a6c742818f38cc54492
wahsandaruwan <wahsandaruwan6@gmail.com>
2021-10-08 23:29:53 +0530

    Added seperate routes file for users

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const app = express()
 
 require('./helpers/mongodb_init')
 
+// Import routes
+const usersRoute = require('./routes/users')
+
 // Middlewares
 // Functions that execute during the lifecycle of a request to the Express server. And we can check the user authentication here.
 // app.use('/users', () => {
 //     console.log("Hello this is users end point!")
 // })
 
-// Routes
+app.use('/users', usersRoute)
+
+// Basic route
 app.get('/', (req, res) => {
     res.send('Hello guys! how are you?')
-})
-
-app.get('/users', (req, res) => {
-    res.send('This is users page!')
 })
 
 // Start listening to the server'
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const router = express.Router();
+
+// Create routes for users
+router.get('/', (req, res) => {
+    res.send('This is users page')
+})
+
+router.get('/premium', (req, res) => {
+    res.send('These are premium users')
+})
+
+module.exports = router
